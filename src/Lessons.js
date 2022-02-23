80440b77bf8861886d29e477bc45f179248c8e64
cssg-edu-team-22 <100238095+cssg-edu-team-22@users.noreply.github.com>
2022-02-22 22:10:08 -0500

    Update Lessons.js

--- src/Lessons.js
+++ src/Lessons.js
@@ -6,22 +6,22 @@ const Lessons = () => {
       path: "/lesson-one",
       name: "Lesson 1: Deploy Your Own React Site + Learn Git",
     },
-    {
-      path: "/lesson-two",
-      name: "Lesson 2: Build a To Do App",
-    },
-    {
-      path: "/lesson-three",
-      name: "Lesson 3: Start Fetching Data Asynchronously",
-    },
-    {
-      path: "/lesson-four",
-      name: "Lesson 4: Introducing Firebase!",
-    },
-    {
-      path: "/lesson-five",
-      name: "Lesson 5: How To: Group Projects!",
-    },
+//    {
+//      path: "/lesson-two",
+//      name: "Lesson 2: Build a To Do App",
+//    },
+//    {
+//      path: "/lesson-three",
+//      name: "Lesson 3: Start Fetching Data Asynchronously",
+//    },
+//    {
+//      path: "/lesson-four",
+//      name: "Lesson 4: Introducing Firebase!",
+//    },
+//    {
+//      path: "/lesson-five",
+//      name: "Lesson 5: How To: Group Projects!",
+//    },
   ];
 
   return (
